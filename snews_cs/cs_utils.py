9cd22f294ede1705c5d4496b8fcbe6ad09eff661
storreslara <sebastiantorreslara17@gmail.com>
2022-02-15 14:53:53 -0600

    added new data args to alert data

--- snews_cs/cs_utils.py
+++ snews_cs/cs_utils.py
@@ -46,22 +46,6 @@ class TimeStuff:
         return datetime.strptime(nu_time, fmt)
 
 
-def isnotebook():
-    """ Tell if the script is running on a notebook
-
-    """
-    try:
-        shell = get_ipython().__class__.__name__
-        if shell == 'ZMQInteractiveShell':
-            return True  # Jupyter notebook or qtconsole
-        elif shell == 'TerminalInteractiveShell':
-            return False  # Terminal running IPython
-        else:
-            return False  # Other type (?)
-    except NameError:
-        return False  # Probably standard Python interpreter
-
-
 # TODO: needs work
 def get_logger(scriptname, logfile_name):
     """ Logger
@@ -84,20 +68,10 @@ def get_logger(scriptname, logfile_name):
     return logger
 
 
-def display_gif():
-    """ Some fun method to display an alert gif
-        If running on notebook
-
-    """
-    if isnotebook():
-        from IPython.display import HTML, display
-        giphy_snews = "https://raw.githubusercontent.com/SNEWS2/hop-SNalert-app/snews2_dev/hop_comms/auxiliary/snalert.gif"
-        display(HTML(f'<img src={giphy_snews}>'))
-
 
 # TODO: Change to SNEWS_PT struc
 def data_cs_alert(p_vals=None, nu_times=None,
-                  detector_names=None, ):
+                  detector_names=None, p_val_avg=None, sub_list_num=None):
     """ Default alert message data
         
         Parameters
@@ -115,6 +89,6 @@ def data_cs_alert(p_vals=None, nu_times=None,
                 dictionary of the complete alert data
 
     """
-    keys = ['p_vals', 'neutrino_times', 'detector_names']
-    values = [p_vals, nu_times, detector_names, ]
+    keys = ['p_vals', 'neutrino_times', 'detector_names','p value average','sub_list_num']
+    values = [p_vals, nu_times, detector_names, p_val_avg, sub_list_num]
     return dict(zip(keys, values))
