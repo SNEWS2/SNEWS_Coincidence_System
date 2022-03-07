d6be727d4592386f3381db5f36fb66dd7dc9ca38
Melih Kara <kara@kit.edu>
2022-03-07 21:50:34 +0100

    clean CLI

--- snews_cs/__main__.py
+++ snews_cs/__main__.py
@@ -46,53 +46,22 @@ def run_coincidence(local):
         click.secho(f'\n{"="*30}DONE{"="*30}', fg='white', bg='green')
 
 
-@main.command()
-@click.option('--test/--no-test', default=True, show_default='True', help='should the bot tag the channel')
-def run_slack_bot(test):
-    """
-    """
-    test = 1 if test else 0
-    os.system(f'python3 snews_bot.py {test}')
+# Called automatically inside the coincidence script 
+# @main.command()
+# @click.option('--test/--no-test', default=True, show_default='True', help='should the bot tag the channel')
+# def run_slack_bot(test):
+#     """
+#     """
+#     test = 1 if test else 0
+#     os.system(f'python3 snews_bot.py {test}')
 
-
-@main.command()
-@click.option('--rate','-r', default=1, nargs=1, help='rate to send observation messages in sec')
-@click.option('--alert_probability','-p', default=0.2, nargs=1, help='probability for an alert')
-@click.pass_context
-def simulate(ctx, rate, alert_probability):
-    """ Simulate Observation Messages
-    """
-    import numpy as np
-    import time
-    from hop import Stream
-    times = cs_utils.TimeStuff()
-    cs_utils.set_env(ctx.obj['env'])
-    obs_broker = os.getenv("OBSERVATION_TOPIC")
-    stream = Stream(until_eos=True, auth=True)
-    click.secho(f'Simulating observation messages every {rate} sec\n\
-        with a {alert_probability*100}% alert probability ', fg='blue', bg='white', bold=True)
-    try:
-        with stream.open(obs_broker, 'w') as s:
-            while True:
-                detector = randomly_select_detector()
-                if np.random.random() < alert_probability:
-                    msg = get_simulated_message(times.get_snews_time('%H:%M:%S:%f'), detector)
-                    s.write(msg)
-                    display_message(msg)
-
-                time.sleep(rate)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        click.secho(f'\n{"="*30}DONE{"="*30}', fg='white', bg='green')
-
-
-def display_message(message):
-    click.secho(f'{"-" * 57}', fg='bright_blue')
-    if message['_id'].split('_')[1] == 'FalseOBS':
-        click.secho("It's okay, we all make mistakes".upper(), fg='magenta')
-    for k, v in message.items():
-        print(f'{k:<20s}:{v}')
+# Format changed, would not work. Not needed
+# def display_message(message):
+#     click.secho(f'{"-" * 57}', fg='bright_blue')
+#     if message['_id'].split('_')[1] == 'FalseOBS':
+#         click.secho("It's okay, we all make mistakes".upper(), fg='magenta')
+#     for k, v in message.items():
+#         print(f'{k:<20s}:{v}')
 
 
 if __name__ == "__main__":
