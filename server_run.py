95da242b78d802b4d5f11549846aa2dede2d84c2
Sebastian Torres-Lara <sebastiantorreslara17@gmail.com>
2022-08-01 15:24:42 -0400

    Updated server_run.py

--- server_run.py
+++ server_run.py
@@ -1,9 +1,10 @@
 from snews_cs.snews_coinc import CoincDecider
 import hop
+
 # from  snews_cs import  _version
 
 if __name__ == '__main__':
     print(f'hop version: {hop.__version__}')
     # print(f'SNEWS CS version: {_version.__version__}')
-    coinc = CoincDecider(use_local_db=True, drop_db=True, firedrill_mode=False,send_email=True, server_tag='NOT_SNEWS')
+    coinc = CoincDecider(use_local_db=True, drop_db=True, firedrill_mode=False, send_email=True, server_tag='NOT_SNEWS')
     coinc.run_coincidence()
