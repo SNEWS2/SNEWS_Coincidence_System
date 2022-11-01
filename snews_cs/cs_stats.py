436afec6ce6ac67a981c2bb6c3153db047671d27
Sebastian Torres-Lara <sebastiantorreslara17@gmail.com>
2022-11-01 12:36:10 -0500

    Updated stats (convert n-fold coincidence to n/week)

--- snews_cs/cs_stats.py
+++ snews_cs/cs_stats.py
@@ -18,9 +18,10 @@ def cache_false_alarm_rate(cache_sub_list, hb_cache):
         probability that SNEWS alert is  a false alarm
 
     """
-    num_coinc_detectors = len(cache_sub_list['detector_name'])
+    # convert our n-fold coicidence from n-fold/day to n-fold/per week
+    num_coinc_detectors = len(cache_sub_list['detector_name']) * (1/7)
     num_detectors_online = len(hb_cache)
-    mu = 1 * num_detectors_online  # expected number of false coincidence for a week
+    mu = 1 * num_detectors_online   # expected number of false coincidence for a week
     prob_false_alarm_rate = poisson.pmf(k=num_coinc_detectors, mu=mu)
     return np.round(prob_false_alarm_rate, decimals=5)
 
