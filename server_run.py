839a8182419a99e1a49f73feda2ac9abe9a3646b
Melih Kara <kara@kit.edu>
2022-11-21 14:12:42 +0100

    fix function call

--- server_run.py
+++ server_run.py
@@ -1,4 +1,4 @@
-from snews_cs.snews_coinc import CoincDecider
+from snews_cs.snews_coinc_v2 import CoincidenceDistributor
 import hop
 from socket import gethostname
 
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     print(f'hop version: {hop.__version__}')
     # print(f'SNEWS CS version: {_version.__version__}')
     server_tag = gethostname()
-    coinc = CoincDecider(use_local_db=True, drop_db=True, firedrill_mode=False, send_email=True, server_tag=server_tag)
+    coinc = CoincidenceDistributor(use_local_db=True, drop_db=True, firedrill_mode=False, send_email=True, server_tag=server_tag)
     coinc.run_coincidence()
