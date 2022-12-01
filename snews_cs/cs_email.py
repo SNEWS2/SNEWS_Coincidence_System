9a71321efeda71b8664550a0df2a424af950be52
Melih Kara <kara@kit.edu>
2022-12-01 10:51:38 +0100

    mail placeholders

--- snews_cs/cs_email.py
+++ snews_cs/cs_email.py
@@ -1,9 +1,8 @@
 
 # https://unix.stackexchange.com/questions/381131/simplest-way-to-send-mail-with-image-attachment-from-command-line-using-gmail
 
-import os
+import os, json
 from datetime import datetime
-import json
 from .core.logging import getLogger
 
 log = getLogger(__name__)
@@ -29,28 +28,25 @@ def send_email(alert_content):
     log.info(f"\t\t> SNEWS Alert mail was sent at {datetime.utcnow().isoformat()} to {emails}")
 
 
-# def send_mailto_contact(detector_name, content):
-#     contacts = contact_list["emails"][detector_name]
-#     if len(contacts)>0:
-#         for contact in contacts:
-#             os.system(f'echo "{content}"| mail -s "SNEWS COINCIDENCE {datetime.utcnow().isoformat()}" {contact}')
-#         log.info(f"\t\t> SNEWS Feedback mail was sent at {datetime.utcnow().isoformat()} to {', '.join(contacts)}")
-
-
 base_msg="'smtps://$USER:$PASSWORD@smtp.gmail.com' mutt " \
          "  -F /dev/null " \
-         "  -e 'set from=%sender' " \
+         "  -e 'set from={sender}' " \
          "  -e 'set smtp_url=$SMTP_URL' " \
          "  -s 'SNEWS Server Feedback for %detector' " \
-         "  -a %attachment --  " \
-         "  %contact << EOM " \
-         " Dear %detector, Attached %attachment please find the feedback information," \
-         " %message_content" \
+         "  -a {attachment} --  " \
+         "  {contact} << EOM " \
+         " Dear {detector}, Attached {attachment} please find the feedback information," \
+         " {message_content}" \
          " Provided by the SNEWS server. --Cheers EOM"
 
-def send_feedback_mail(detector, attachment, message_content=None):
-    contacts = contact_list["emails"][detector]
-    mail = ""
+def send_feedback_mail(detector, attachment, message_content=None, given_contact=None):
+    """ Send feedback email to authorized, requested users
+    """
+    if type(given_contact) != list:
+        contacts = list(given_contact)
+    else:
+        contacts = given_contact
+
     message_content = message_content or ""
     if len(contacts) > 0:
         for contact in contacts:
@@ -59,24 +55,25 @@ def send_feedback_mail(detector, attachment, message_content=None):
                                    attachment=os.path.join(beats_path, attachment),
                                    contact=contact,
                                    message_content=message_content)
-            os.system(mail)
-        log.info(f"\t\t> Feedback Sent to {contacts}\n"
-                 f"\n{mail}\n")
+            # os.system(mail)
+            print(mail)
+        log.info(f"\t\t> Feedback Sent to {contacts} for {detector}")
+    else:
+        log.info(f"\t\t> Feedback mail is requested for {detector}. However, there are no contacts added.")
 
 
 base_warning="'smtps://user:password@smtp.gmail.com' mutt " \
              "  -F /dev/null " \
-             "  -e 'set from=%sender' " \
+             "  -e 'set from={sender}' " \
              "  -e 'set smtp_url=$SMTP_URL' " \
-             "  -s 'SNEWS Server Heartbeat for %detector is skipped!' " \
-             "  %contact << EOM " \
-             " Dear %detector, " \
-             " %message_content" \
+             "  -s 'SNEWS Server Heartbeat for {detector} is skipped!' " \
+             "  {contact} << EOM " \
+             " Dear {detector}, " \
+             " {message_content}" \
              " Provided by the SNEWS server. --Cheers EOM"
 
 def send_warning_mail(detector, message_content=None):
-    contacts = contact_list["emails"][detector]
-    mail = ""
+    contacts = contact_list[detector]["emails"]
     message_content = message_content or ""
     if len(contacts) > 0:
         for contact in contacts:
@@ -84,7 +81,49 @@ def send_warning_mail(detector, message_content=None):
                                    detector=detector,
                                    contact=contact,
                                    message_content=message_content)
-            os.system(mail)
-        log.info(f"\t\t> Warning Sent to {contacts}\n"
-                 f"\n{mail}\n")
+            # os.system(mail)
+            print(mail)
+        log.info(f"\t\t> Warning Sent to {contacts}\n")
 
+#### sudo apt-get install sendmail
+#################### TEST
+test_warning="'smtps://{user}:{password}@smtp.gmail.com' mutt " \
+             "  -F /dev/null " \
+             "  -e 'set from={sender}' " \
+             "  -e 'set smtp_url=$SMTP_URL' " \
+             "  -s 'SNEWS Server Heartbeat for %detector is skipped!' " \
+             "  Melih << EOM " \
+             " Dear {detector}, " \
+             " {message_content}" \
+             " Provided by the SNEWS server. --Cheers EOM"
+
+def send_test_mail(user, password, sender='Melih'):
+    mail = base_msg.format(user=user,
+                           password=password,
+                           sender=sender,
+                           detector="XENONnT",
+                           message_content="Random message_content")
+    # os.system(mail)
+
+    # Import smtplib for the actual sending function
+    import smtplib
+    # Import the email modules we'll need
+    from email.mime.text import MIMEText
+
+    # Open a plain text file for reading.  For this example, assume that
+    # the text file contains only ASCII characters.
+    # with open(textfile, 'rb') as fp:
+        # Create a text/plain message
+    msg = MIMEText("Some random text") #MIMEText(fp.read())
+
+    # me == the sender's email address
+    # you == the recipient's email address
+    msg['Subject'] = 'The contents of some file' #% textfile
+    msg['From'] = "kara@kit.edu"
+    msg['To'] = "mlh-kara@hotmail.com"
+
+    # Send the message via our own SMTP server, but don't include the
+    # envelope header.
+    s = smtplib.SMTP('localhost')
+    s.sendmail("kara@kit.edu", ["mlh-kara@hotmail.com"], msg.as_string())
+    s.quit()
