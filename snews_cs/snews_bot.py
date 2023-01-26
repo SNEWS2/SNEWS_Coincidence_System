99ede6a9322e72e68c9a685a217daeecafcf25cc
Melih Kara <kara@kit.edu>
2023-01-26 15:00:02 +0100

    make nicer slack posts

--- snews_cs/snews_bot.py
+++ snews_cs/snews_bot.py
@@ -9,6 +9,7 @@ import os
 from . import cs_utils
 from .cs_alert_schema import CoincidenceTierAlert
 import warnings
+import pandas as pd
 
 cs_utils.set_env()
 slack_token = os.getenv('SLACK_TOKEN')
@@ -22,20 +23,26 @@ def get_image(is_test, alert_data, topic):
     ## parse input
     tag = '<!here>\n' if not is_test else '\n'
     test = "TEST" if is_test else ""
-    topic_str = f"\n> Broker: {topic.center(50,'-')}\n"
+    topic_str = f"\n> Broker: {topic.center(50,'-')}"
 
-    alert_data = alert_data or dict(server_tag="Unknown Server", _id="Unknown ID", false_alarm_prob="Unknown")
-    update = True if "UPDATE" in alert_data['_id'] else False
-    update = "UPDATE" if update else ""
+    alert_data = alert_data or dict(server_tag="Unknown Server",
+                                    alert_type="Unkown",
+                                    _id="Unknown ID",
+                                    false_alarm_prob="Unknown")
+    # update = True if "UPDATE" in alert_data['_id'] else False
+    # update = "UPDATE" if update else ""
+    alert_type = alert_data['alert_type']
     server = alert_data['server_tag']
     falseprob = alert_data['False Alarm Prob']
 
-    header = f"{test} *SUPERNOVA ALERT* {update}".center(60, '=')+topic_str+f"{tag}" + \
+    header = f"{test} *SUPERNOVA ALERT* {alert_type}".center(60, '=')+topic_str+f"{tag}" + \
              f"> False Alarm Probability= *{falseprob}*\n> Issued from {server}"
     giflink = "https://raw.githubusercontent.com/SNEWS2/SNEWS_Coincidence_System/main/snews_cs/auxiliary/snalert.gif"
-    updatelink = "https://raw.githubusercontent.com/SNEWS2/SNEWS_Coincidence_System/main/snews_cs/auxiliary/update_image.png"
+    retractlink = "https://www.shutterstock.com/image-vector/ooops-word-bubble-pop-art-600w-408777070.jpg"
+    updatelink = "https://www.shutterstock.com/image-vector/vector-illustration-modern-label-new-600w-1520423249.jpg"
+    # updatelink = "https://raw.githubusercontent.com/SNEWS2/SNEWS_Coincidence_System/main/snews_cs/auxiliary/update_image.png"
     #"https://www.ris.world/wp-content/uploads/2018/09/update.jpg"
-    sendlink = updatelink if update else giflink
+    sendlink = giflink if alert_type=="NEW_MESSAGE" else (updatelink if alert_type=="UPDATE" else retractlink)
 
     im = \
         [{
@@ -75,11 +82,14 @@ def get_image(is_test, alert_data, topic):
 # It's a best practice to always provide a `text` argument when posting a message.
 # The `text` argument is used in places where content cannot be rendered such as: system push notifications,
 # assistive technology such as screen readers, etc.
-def send_table(df, is_test, alert_data, topic):
-    alert = alert_schema.get_cs_alert_schema(data=alert_data)
-    df_simplified = df.copy()
-    df_simplified = df_simplified[["detector_name", "neutrino_time", "nu_delta_t", "p_val", "meta", "sub_list_num"]]
-    df_simplified.sort_values("nu_delta_t", inplace=True)
+def send_table(alert_data, alert, is_test, topic):
+    """ send warning on slack.
+        Both alert_data (dictionary with info from each detector)
+        and the alert (single dict with collected info) are required
+    """
+    df = pd.DataFrame.from_dict(alert_data)
+    df_simplified = df[["detector_names", "neutrino_times", "p_vals"]]
+    df_simplified.sort_values("neutrino_times", inplace=True)
     table = df_simplified.to_markdown() # tablefmt="grid"
     image_block = get_image(is_test, alert, topic)
     with warnings.catch_warnings():
