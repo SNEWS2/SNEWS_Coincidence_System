06670a81e8bb87d5cb490212951f36d4a78f75f5
Melih Kara <kara@kit.edu>
2023-01-26 11:06:14 +0100

    minor

--- server_run.py
+++ server_run.py
@@ -8,5 +8,9 @@ if __name__ == '__main__':
     print(f'hop version: {hop.__version__}')
     # print(f'SNEWS CS version: {_version.__version__}')
     server_tag = gethostname()
-    coinc = CoincidenceDistributor(use_local_db=True, drop_db=True, firedrill_mode=False, send_email=True, server_tag=server_tag)
+    coinc = CoincidenceDistributor(use_local_db=True,
+                                   drop_db=True,
+                                   firedrill_mode=False,
+                                   server_tag=server_tag,
+                                   send_email=True)
     coinc.run_coincidence()
