c1412901943da6a0bd01f092ef954b4fd979113c
Christopher Orr <chris.orr+github@gmail.com>
2023-08-25 22:59:55 -0400

    adding a first pass at using smtplib.  committing for the night.

--- snews_cs/cs_email.py
+++ snews_cs/cs_email.py
@@ -6,6 +6,20 @@ from datetime import datetime
 from .core.logging import getLogger
 from .snews_hb import beats_path
 
+### smtplib bits
+# Import smtplib for the actual sending function
+import smtplib
+
+# For guessing MIME type
+import mimetypes
+
+# Import the email modules we'll need
+import email
+import email.mime.text
+import email.mime.application
+from email.mime.multipart import MIMEMultipart
+###
+
 log = getLogger(__name__)
 
 sender = os.getenv("snews_sender_email")
@@ -28,6 +42,37 @@ def send_email(alert_content):
     os.system( f'echo "{pretty_alert}"| mail -s "SNEWS COINCIDENCE {datetime.utcnow().isoformat()}" {emails}')
     log.info(f"\t\t> SNEWS Alert mail was sent at {datetime.utcnow().isoformat()} to {emails}")
 
+###  First pass at using smtplib.
+###  Note that this is sample code.
+def _smtp_sender(mails)
+    # Create a text/plain message
+    msg = email.mime.multipart.MIMEMultipart()
+    msg['Subject'] = 'This is a test'
+    msg['From'] = 'cjorr@purdue.edu'
+    msg['To'] = 'cjorr@purdue.edu'
+
+    # The main body is just another attachment
+    body = email.mime.text.MIMEText("""Hello, how are you? I am fine.
+    This is a rather nice letter, don't you think?""")
+    msg.attach(body)
+
+    # PDF attachment
+   filename='resume.pdf'
+   fp=open(filename,'rb')
+   att = email.mime.application.MIMEApplication(fp.read(),_subtype="pdf")
+   fp.close()
+   att.add_header('Content-Disposition','attachment',filename=filename)
+   msg.attach(att)
+
+   # send via Gmail server
+   # NOTE: my ISP, Centurylink, seems to be automatically rewriting
+   # port 25 packets to be port 587 and it is trashing port 587 packets.
+   # So, I use the default port 25, but I authenticate.
+   s = smtplib.SMTP('smtp.purdue.edu')
+   #s.starttls()
+   #s.login('xyz@gmail.com','xyzpassword')
+   s.sendmail('cjorr@purdue.edu',['cjorr@purdue.edu'], msg.as_string())
+   s.quit()
 
 def _mail_sender(mails):
     """ Send the generated emails via s-nail
@@ -94,6 +139,3 @@ def send_warning_mail(detector, message_content=None):
             out = _mail_sender([mail_regular])
     else:
         log.info(f"\t\t> Warning is triggered for {detector}. However, there are no contacts added.")
-
-#### sudo apt-get install sendmail
-
