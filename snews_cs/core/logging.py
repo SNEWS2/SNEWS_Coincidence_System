f99c50fac2510dbd1dfda7b0b5a2ff07dcd0b632
Mark Linvill <36269251+mlinvill@users.noreply.github.com>
2024-08-08 11:01:06 -0400

    Add SNEWPlog kafka logging handler.

--- snews_cs/core/logging.py
+++ snews_cs/core/logging.py
@@ -1,5 +1,5 @@
 """
-Modified version of Geoffrey Letner's python 201 logger.
+Modified version of Geoffrey Lentner's python 201 logger.
 
 Ref: https://python-tutorial.dev/201/tutorial/logging.html
 """
@@ -7,9 +7,11 @@ import os
 import time
 from datetime import date
 from socket import gethostname
+from hop import Stream
 from logging import (
     getLogger,
     NullHandler,
+    Handler,
     Formatter,
     FileHandler,
     DEBUG,
@@ -18,14 +20,26 @@ from logging import (
     ERROR,
     CRITICAL,
 )
+from . import cs_utils
+
+cs_utils.set_env(env_path)
+broker = os.getenv("HOP_BROKER")
+topic = os.getenv("SNEWPLOG_TOPIC")
 
 HOST = gethostname()
-
 log_date = date.today().strftime("%Y-%m-%d")
-log_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../logs"))
+log_dir = os.getenv('SNEWSLOG')
 
+if not log_dir:
+    log_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../logs"))
+
+# These should be exclusive, one or the other: fh or klh.
 log_file = f"{log_dir}/snews_cs.log"
 fh = FileHandler(log_file)
+#
+klh = SNEWPlog(broker, topic, auth)
+#
+
 
 formatter = Formatter(
     f"%(asctime)s on {HOST}\n" f"  %(levelname)s [%(name)s] %(message)s",
@@ -54,3 +68,25 @@ def initialize_logging(level):
         logger.addHandler(fh)
         logger.setLevel(levels.get(level))
         logger.propagate = False
+
+
+class SNEWPlog(logging.Handler):
+    """ Implement logging over kafka topic
+    """
+
+    def __init__(self, host: str, topic: str, auth: str=None):
+        logging.Handler.__init__(self)
+        self.auth = auth
+        uri = f"{host}/{topic}"
+        self.producer = Stream(until_eos=True, auth=self.auth).open(uri, "w")
+
+    def emit(self, record):
+        if record.name == 'kafka':
+            return
+
+        msg = self.format(record)
+        self.producer.write(msg)
+
+    def close(self):
+        self.producer.close()
+        logging.Handler.close(self)
