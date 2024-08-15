c14d242f76cc8cc519f5bd1d7406572272a272a9
Segev BenZvi <segev.benzvi@rochester.edu>
2024-08-15 18:38:12 -0500

    Update setup.

--- setup.py
+++ setup.py
@@ -3,41 +3,51 @@ import re
 
 from setuptools import find_packages, setup
 
-# read in README
-this_dir = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_dir, 'README.md'), 'rb') as f:
-    long_description = f.read().decode().strip()
-
-# load version
+#- Begin setup
+setup_keywords = dict()
+#
+setup_keywords['name'] = 'snews_cs'
+setup_keywords['description'] = 'An alert application for supernovae neutrino coincidences'
+setup_keywords['author'] = 'SNEWS 2.0 Collaboration',
+setup_keywords['author_email'] = 'snews2.0@lists.bnl.gov',
+setup_keywords['license'] = 'BSD'
+setup_keywords['url'] = 'https://github.com/SNEWS2/SNEWS_Coincidence_System'
+#
+#- version
+setup_keywords['version'] = ''
 with open("snews_cs/_version.py", "r") as f:
     version_file = f.read()
-version_match = re.search(r"^version = ['\"]([^'\"]*)['\"]", version_file, re.M)
-version = version_match.group(1)
-
-# requirements
-install_requires = []
-    # "hop-client=0.8.0",
-    # "jsonschema=4.4.0",
-    # "numpy=1.26.3",
-    # "pymongo=4.6.1",
-    # "python-dotenv=0.19.2",
-    # "pandas=2.2.0",
-    # "slack-sdk=3.26.2",
-# ]
-
+    version_match = re.search(r"^version = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    version = version_match.group(1)
+    setup_keywords['version'] = version
+#
+#- Use README.md as a long description
+if os.path.exists('README.md'):
+    with open('README.md', 'rb') as readme:
+        setup_keywords['long_description'] = readme.read().decode().strip()
+    setup_keywords['long_description_content_type'] = 'text/markdown'
+#
+#- Other keywords for setup
+setup_keywords['provides'] = [setup_keywords['name']]
+setup_keywords['python_requires'] = '>=3.11'
+setup_keywords['zip_safe'] = False
+setup_keywords['setup_requires'] = ['pbr']
+setup_keywords['pbr'] = True
+setup_keywords['packages'] = find_packages()
+setup_keywords['include_package_data'] = True
+setup_keywords['package_dir'] = {'': '.'}
+setup_keywords['package_data'] = {'' : ['etc/*.json', 'etc/*.env']}
+setup_keywords['test_suite'] = 'snews_cs.test.snews_cs_test_suite.snews_cs_test_suite'
+#
+#- Requirements
+requires = []
 with open('requirements.txt', 'r') as f:
     for line in f:
         if line.strip():
-            install_requires.append(line.strip())
-
-# def read_requirements():
-#     with open('requirements.txt') as req:
-#         content = req.read()
-#         requirements = content.split('\n')
-#     return install_requires.append(requirements)
-
-
-extras_require = {
+            requires.append(line.strip())
+#
+setup_keywords['install_requires'] = requires
+setup_keywords['extras_require'] = {
     'dev': [
         'autopep8',
         'flake8',
@@ -55,49 +65,30 @@ extras_require = {
         'sphinxcontrib-programoutput'
     ],
 }
-
-setup(
-    name='snews_cs',
-    version=version,
-    description='An alert application for observing supernovas.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/SNEWS2/SNEWS_Coincidence_System.git',
-    author='Sebastian Torres-Lara, Melih Kara',
-    author_email='sebastiantorreslara17@gmail.com, karamel.itu@gmail.com',
-    license='BSD 3-Clause',
-    setup_requires=['pbr'],
-    pbr=True,
-    packages=find_packages(),
-    include_package_data=True,
-
-    entry_points={
-        'console_scripts': [
-            'snews_cs = snews_cs.__main__:main',
-        ],
-    },
-
-    python_requires='>=3.9.*',
-    install_requires=install_requires, #read_requirements(),
-    extras_require=extras_require,
-
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Astronomy',
-        'Topic :: Scientific/Engineering :: Physics',
-        'Operating System :: POSIX',
-        'Operating System :: Unix',
-        'Operating System :: MacOS',
-        'License :: OSI Approved :: BSD License',
+#
+#- Command-line access
+setup_keywords['entry_points'] = {
+    'console_scripts': [
+        'snews_cs = snews_cs.__main__:main',
     ],
-    
-    package_dir = {'' : ''},
-    test_suite='snews_cs.test.snews_cs_test_suite.snews_cs_test_suite',
-
-)
+}
+#
+#- Classifiers list
+setup_keywords['classifiers'] = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Astronomy',
+    'Topic :: Scientific/Engineering :: Physics',
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Operating System :: MacOS',
+    'License :: OSI Approved :: BSD License',
+]
+#
+#- Run the setup command:
+setup(**setup_keywords)
