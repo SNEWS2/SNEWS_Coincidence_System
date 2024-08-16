d2b1fc11f5bf04b0d325bc7b90fe8c147d2bd353
KaraMelih <kara@kit.edu>
2024-08-16 11:58:21 -0500

    turn off tests for now

--- snews_cs/test/test_01_connection_to_server.py
+++ snews_cs/test/test_01_connection_to_server.py
@@ -4,18 +4,18 @@
 import unittest
 
 import snews_cs
-import snews_pt.remote_commands as sptrc
+# import snews_pt.remote_commands as sptrc
 
 import io
 import contextlib
 
-class TestServer(unittest.TestCase):
-    def test_connection(self):
-        f = io.StringIO()
-        with contextlib.redirect_stdout(f):
-            #- Connect to server
-            sptrc.test_connection(detector_name='XENONnT', firedrill=False, start_at='LATEST', patience=8)
-
-            #- Check the output message; it should say "You (XENONnT) have a connection"
-            confirm_msg = 'You (XENONnT) have a connection to the server'
-            self.assertTrue(confirm_msg in f.getvalue())
+# class TestServer(unittest.TestCase):
+#     def test_connection(self):
+#         f = io.StringIO()
+#         with contextlib.redirect_stdout(f):
+#             #- Connect to server
+#             sptrc.test_connection(detector_name='XENONnT', firedrill=False, start_at='LATEST', patience=8)
+#
+#             #- Check the output message; it should say "You (XENONnT) have a connection"
+#             confirm_msg = 'You (XENONnT) have a connection to the server'
+#             self.assertTrue(confirm_msg in f.getvalue())
