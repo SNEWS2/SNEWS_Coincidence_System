86a9c9b7316575445d7d5d29af2c2169feaa137d
Cecil Gregg <cg4express@gmail.com>
2025-02-22 12:54:04 -0500

    lint error on init

--- snews_cs/__init__.py
+++ snews_cs/__init__.py
@@ -1,3 +1,17 @@
 from .core.logging import initialize_logging
+import pandas as pd
 
 initialize_logging("debug")
+
+pd.options.mode.chained_assignment = None
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    pass
+
+
+def __getattr__(name):
+    if name == "__version__":
+        return __version__
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
