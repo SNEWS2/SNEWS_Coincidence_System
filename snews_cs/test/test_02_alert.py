f1473d7dd554a963329c26c171b4c9b25f3f720b
Ricardo <20727577+ricmperes@users.noreply.github.com>
2025-07-29 00:18:40 +0100

    FIx failing CI test:   - Change `messages.create_messages` to `messages.CoincidenceTierMessage`   - Send messages after start listening to stream

--- snews_cs/test/test_02_alert.py
+++ snews_cs/test/test_02_alert.py
@@ -8,6 +8,7 @@ import unittest
 from hop import Stream
 from hop.io import StartPosition
 from snews.models import messages
+from snews_pt.messages import Publisher
 from snews_pt.remote_commands import reset_cache
 
 from snews_cs.snews_coinc import CoincidenceDistributor
@@ -15,29 +16,29 @@ from snews_cs.snews_coinc import CoincidenceDistributor
 
 class TestServer(unittest.TestCase):
     def test_alerts(self):
-        coin1 = messages.create_messages(detector_name='KamLAND',
-                   tier='CoincidenceTier',
-                   machine_time='2012-06-09T15:30:00.000501',
-                   neutrino_time_utc='2012-06-09T15:31:07.465011',
-                   p_val=0.98,
-                   is_test=True,
-                   is_firedrill=False)
+        coin1 = messages.CoincidenceTierMessage(detector_name='KamLAND',
+                               neutrino_time_utc='2012-12-21T15:31:08.465011',
+                               p_val=0.98,
+                               is_test=True,
+                               is_firedrill=False)
 
-        coin2 = messages.create_messages(detector_name='XENONnT',
-                   tier='CoincidenceTier',
-                   machine_time='2012-06-09T15:30:00.000501',
-                   neutrino_time_utc='2012-06-09T15:31:07.465011',
-                   p_val=0.98,
-                   is_test=True,
-                   is_firedrill=False)
+        coin2 = messages.CoincidenceTierMessage(detector_name='XENONnT',
+                                    neutrino_time_utc='2012-12-21T15:31:07.465011',
+                                    p_val=0.98,
+                                    is_test=True,
+                                    is_firedrill=False)
 
         # make sure the test cache is empty
         reset_cache(is_test=True)
 
+        publisher = Publisher("kafka://kafka.scimma.org/snews.experiments-test")
+
         # First, send two coinciding messages
         for coin in [coin1, coin2]:
             try:
-                coin.send_messages()
+                publisher.add_message(coin1)
+                publisher.add_message(coin2)
+
             except Exception as exc:
                 print('test_alert test failed trying to send messages with SNEWSMessageBuilder.send_messages() !\n')
                 assert False, f"Exception raised:\n {exc}"
@@ -47,30 +48,33 @@ class TestServer(unittest.TestCase):
 
         # Next, manually open the stream and search for coincidences
         # this tests the coincidence logic ALREADY RUNNING on the server
-        default_connection_topic = "kafka://kafka.scimma.org/snews.connection-testing"
-        test_alert_topic = os.getenv("CONNECTION_TEST_TOPIC", default_connection_topic)
+        default_connection_topic = "kafka://kafka.scimma.org/snews.experiments-test"
+        test_alert_topic = os.getenv("CONNECTION_TEST_TOPIC", 
+                                     default_connection_topic)
 
         _start_at = StartPosition.LATEST #if start_at=="LATEST" else StartPosition.EARLIEST
         substream = Stream(until_eos=True, auth=True, start_at=_start_at)
 
-        message_expected = {"False Alarm Prob": "N/A",
-                            "id": "SNEWS_Coincidence_ALERT XXXXXXXXXXX",
-                            "alert_type": "TEST COINC_MSG",
-                            "detector_names": ["KamLAND", "XENONnT"],
-                            "neutrino_times": ["2012-06-09T15:31:08.891011000", "2012-06-09T15:31:07.465011000"],
-                            "p_values": [0.98, 0.98],
-                            "p_values average": 0.98,
-                            "sent_time": "XXXXXXX",
-                            "server_tag": "XXXXXXXX",
-                            "sub list number": 0}
+        message_expected = {'False Alarm Prob': 'N/A',
+                            'detector_names': ['XENONnT', 'DS-20K'],
+                            'p_values': [0.98, 0.98],
+                            'neutrino_times': ['2030-01-01T12:34:45.678999000Z',
+                            '2030-01-01T12:34:47.678999000Z'],
+                            'p_values average': 0.98
+                            }
 
-        fields_must_match = ["False Alarm Prob", "alert_type", "detector_names", "neutrino_times", "p_values", "p_values average"]
+        fields_must_match = ["False Alarm Prob", "alert_type", 
+                             "detector_names", "neutrino_times", 
+                             "p_values", "p_values average"]
 
         with substream.open(test_alert_topic, "r") as ss:
+            publisher.send()
             for read in ss:
                 read = read.content
+                print(read['detector_names'])
                 for field in fields_must_match:
-                    self.assertTrue(read[field] == message_expected[field], f"Field {field} does not match!")
+                    self.assertTrue(read[field] == message_expected[field], 
+                                    f"Field {field} does not match!")
 
         # clear the cache again afterwards
         reset_cache(is_test=True)
