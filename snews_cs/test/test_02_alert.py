3cfd1e1cca84c987d582704e81c52cda10fdee55
Ricardo <20727577+ricmperes@users.noreply.github.com>
2025-07-29 01:37:08 +0100

    Fix CI in alert - try 2:   - Now provide messages that match the template (big ups before)   - Change stream to be open-ended until a message comes   - Added timeout to not be looking for messages that enver come forever

--- snews_cs/test/test_02_alert.py
+++ snews_cs/test/test_02_alert.py
@@ -3,6 +3,7 @@
 """
 
 import os
+import signal
 import unittest
 
 from hop import Stream
@@ -15,6 +16,10 @@ from snews_cs.snews_coinc import CoincidenceDistributor
 
 
 class TestServer(unittest.TestCase):
+    def timeout_handler(self, signum, frame):
+        raise TimeoutError("Test timed out waiting for coincidence message.")
+
+    timeout_secs = 30
     def test_alerts(self):
         coin1 = messages.CoincidenceTierMessage(detector_name='KamLAND',
                                neutrino_time_utc='2012-12-21T15:31:08.465011',
@@ -31,41 +36,37 @@ class TestServer(unittest.TestCase):
         # make sure the test cache is empty
         reset_cache(is_test=True)
 
-        publisher = Publisher("kafka://kafka.scimma.org/snews.experiments-test")
-
-        # First, send two coinciding messages
-        for coin in [coin1, coin2]:
-            try:
-                publisher.add_message(coin1)
-                publisher.add_message(coin2)
-
-            except Exception as exc:
-                print('test_alert test failed trying to send messages with SNEWSMessageBuilder.send_messages() !\n')
-                assert False, f"Exception raised:\n {exc}"
+        publisher = Publisher("kafka://kafka.scimma.org/snews.experiments-test")            
+        publisher.add_message(coin1)
+        publisher.add_message(coin2)
 
         # we need to think of a way for github actions to run the server
         # coincidence_searcher = CoincidenceDistributor(env_path='/etc/test-config.env', firedrill_mode=False, server_tag='test')
 
         # Next, manually open the stream and search for coincidences
         # this tests the coincidence logic ALREADY RUNNING on the server
+
         default_connection_topic = "kafka://kafka.scimma.org/snews.experiments-test"
         test_alert_topic = os.getenv("CONNECTION_TEST_TOPIC", 
                                      default_connection_topic)
 
         _start_at = StartPosition.LATEST #if start_at=="LATEST" else StartPosition.EARLIEST
-        substream = Stream(until_eos=True, auth=True, start_at=_start_at)
+        substream = Stream(until_eos=False, auth=True, start_at=_start_at)
 
         message_expected = {'False Alarm Prob': 'N/A',
-                            'detector_names': ['XENONnT', 'DS-20K'],
+                            'detector_names': ['XENONnT', 'KamLAND'],
                             'p_values': [0.98, 0.98],
-                            'neutrino_times': ['2030-01-01T12:34:45.678999000Z',
-                            '2030-01-01T12:34:47.678999000Z'],
+                            'neutrino_times': ['2012-12-21T15:31:07.465011000Z', 
+                                               '2012-12-21T15:31:08.465011000Z'],
                             'p_values average': 0.98
                             }
 
-        fields_must_match = ["False Alarm Prob", "alert_type", 
+        fields_must_match = ["False Alarm Prob", 
                              "detector_names", "neutrino_times", 
                              "p_values", "p_values average"]
+                             
+        signal.signal(signal.SIGALRM, self.timeout_handler)
+        signal.alarm(self.timeout_secs)
 
         with substream.open(test_alert_topic, "r") as ss:
             publisher.send()
@@ -75,6 +76,8 @@ class TestServer(unittest.TestCase):
                 for field in fields_must_match:
                     self.assertTrue(read[field] == message_expected[field], 
                                     f"Field {field} does not match!")
+                break # stop otherwise we run forever (or timeout)
 
-        # clear the cache again afterwards
+        # clear the timeout and alarm cache
+        signal.alarm(0)
         reset_cache(is_test=True)
